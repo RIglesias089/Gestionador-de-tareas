f81d32186ca7e2f4614b473015515d503da4fe68
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-06-24 18:06:50 -0600

    feat(Usuario): implementacion de el GestorWorkspace y las listas para que sea dinamico la cantidad de gestores

--- Gestionador_de_tareas/src/main/java/org/Model/Usuario.java
+++ Gestionador_de_tareas/src/main/java/org/Model/Usuario.java
@@ -1,8 +1,11 @@
 package org.Model;
 
-//Importamos lo necesario
+//importamos el interface
 import org.interfaces.Autenticable;
+import java.util.List;
+import java.util.ArrayList;
 
+//Hacemos que sea una clase abstracta para que no nos de un error
 public abstract class Usuario implements Autenticable {
     //Planteamos los datos que usaremos
     private String id;
@@ -11,7 +14,8 @@ public abstract class Usuario implements Autenticable {
     private String password;
     private Boolean premium; //esto nos serviara para poder ver si es premium o no.
 
-
+    //Al implementar un gestor de workspace, este puede ser uno o varios y debe ser dinamico para que pueda aumentar o disminuir sin restriccion
+    private List<GestorWorkspace> workspace;
 
     //Siempre dentro de la clase, planteamos el contructorq ue inicializara los objetos
     public Usuario(String id, String nombre, String email, String password){
@@ -19,6 +23,8 @@ public abstract class Usuario implements Autenticable {
         this.nombre = nombre;
         this.email = email;
         this.password = password;
+
+        this.workspace = new ArrayList<>(); //el Arraylist hace una lista apoyada en un solo arreglo interno quen nos ayuda a darle dinamismo.
     }
 
     // Aplicamos sobrecarga con otro constructo que nos permitira en el main podamos crear de mas de una forma un Usuario
