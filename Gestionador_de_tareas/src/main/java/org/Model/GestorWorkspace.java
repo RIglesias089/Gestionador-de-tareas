6db48e59a53c36de7cffc5f4d152ce6559bbc074
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-06-25 10:13:49 -0600

    feat: Implementacion de los get y set de los diferentes datos que se encuentran en el gestorworkspace

--- Gestionador_de_tareas/src/main/java/org/Model/GestorWorkspace.java
+++ Gestionador_de_tareas/src/main/java/org/Model/GestorWorkspace.java
@@ -11,11 +11,68 @@ public class GestorWorkspace {
     String descripcion;
 
     private List<Usuario>miembros; //Hacemos que la cantidad de usuarios sean renombradas como los miembros
+    private Usuario creador; //Definimos un objeto de tipo usuario con el rol de creador
+
 
     //Creamos el contructor de el gestor de workspace
-    public GestorWorkspace(String id_gestor, String nombre_gestor, String descripcion){
+    public GestorWorkspace(
+            String id_gestor,
+            String nombre_gestor,
+            String descripcion,
+            List<Usuario> miembros,
+            Usuario creador
+    ) {
+
         this.id_gestor = id_gestor;
         this.nombre_gestor = nombre_gestor;
         this.descripcion = descripcion;
+
+        this.miembros = miembros;
+        this.creador = creador;
     }
+
+    //Hacemos los metodos para la clase
+    public String getId_gestor() {
+        return id_gestor;
+    }
+
+    public void setId_gestor(String id_gestor) {
+        this.id_gestor = id_gestor;
+    }
+
+    public String getNombre_gestor() {
+        return nombre_gestor;
+    }
+
+    public void setNombre_gestor(String nombre_gestor) {
+        this.nombre_gestor = nombre_gestor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //El metodo toma los datos de el lsitado de usuarios y los deveulve como miembros nada mas
+    public List<Usuario> getMiembros() {
+        return miembros;
+    }
+
+    //Reescribe los miembros que extrajo en miembros
+    public void setMiembros(List<Usuario> miembros) {
+        this.miembros = miembros;
+    }
+
+    public Usuario getCreador() {
+        return creador;
+    }
+
+    public void setCreador(Usuario creador) {
+        this.creador = creador;
+    }
+
+
 }
