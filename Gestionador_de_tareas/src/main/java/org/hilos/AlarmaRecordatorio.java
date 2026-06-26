957d98ae98c88b48b53e0937827614214d91f2ba
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-06-26 15:29:27 -0600

    Docs: Implementamos comentarios en el hilo y clase de Alarmarecordatorio

--- Gestionador_de_tareas/src/main/java/org/hilos/AlarmaRecordatorio.java
+++ Gestionador_de_tareas/src/main/java/org/hilos/AlarmaRecordatorio.java
@@ -14,22 +14,23 @@ public class AlarmaRecordatorio implements  Runnable{
     }
 
     @Override
-    public void run() {
+    public void run() { //Sobrescribimos el metodo de run (el cual recordemos es obligatorio al usar un Runnable
 
-        long tiempoEspera = Duration.between(
-                LocalDateTime.now(),
-                recordatorio.getHora()
-        ).toMillis();
+        long tiempoEspera = Duration.between( //Comenzamos a calcular el tiempo de espera para la alarma.
+                //El Duration.between calcula la diferencia entre la hora exacta y la hora programada hasta el milisegundo
+                LocalDateTime.now(), //Toma la hora exacta
+                recordatorio.getHora() // Es la hora programada en el recordatorio
+        ).toMillis(); //convierte la diferencia a milisegundos para terminar
 
-        if(tiempoEspera > 0){
+        if(tiempoEspera > 0){ //Verifica que la hora este en futuro siendo mayor a 0
             try {
-                Thread.sleep(tiempoEspera);
+                Thread.sleep(tiempoEspera); //pone en reposo el tiempoespera por el tiempo que falte para llegar a la hora del recordatorio
             }catch (InterruptedException e){
-                Thread.currentThread().interrupt();
+                Thread.currentThread().interrupt(); //si es interrumpida este tirara un mensaje de qeu fue interrumpido 
                 System.out.println("la alarma fue interrumpida.");
                 return;
             }
         }
-        recordatorio.activarAlarma();
+        recordatorio.activarAlarma(); //llama la alarma
     }
 }
