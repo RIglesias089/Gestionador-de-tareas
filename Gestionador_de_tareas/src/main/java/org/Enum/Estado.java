d36dcee3f0cc386a24e38256f332f6a017e8f39d
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-06-26 01:22:51 -0600

    Feat: Implementacion de el enum Estado

---
+++ Gestionador_de_tareas/src/main/java/org/Enum/Estado.java
@@ -0,0 +1,10 @@
+package org.Enum;
+
+//Asi como con el enum de prioridad hacemos este enum con valores constantes que conocemos para poder
+//poder saber si la tarea en un estado especifico
+public enum Estado {
+    PENDIENTE,
+    EN_PROGRESO,
+    COMPLETADO,
+    CANCELADA
+}
