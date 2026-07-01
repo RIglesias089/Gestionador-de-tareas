19685af799c0cfd1158904005772a624b15d3339
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-01 01:03:05 -0600

    feat: Implementamos la pantalla de login para empezar

--- Gestionador_de_tareas/src/main/java/org/App.java
+++ Gestionador_de_tareas/src/main/java/org/App.java
@@ -16,5 +16,23 @@ public class App extends Application {
         Font.loadFont(
                 getClass().getResourceAsStream("/org/css/fonts/Kurale-Regular.ttf"), 14
         );
+
+        //Cargamos la pantalla de login
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/org/GUI/login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 1280, 720);
+
+        stage.setTitle("SnowList");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
     }
 }
