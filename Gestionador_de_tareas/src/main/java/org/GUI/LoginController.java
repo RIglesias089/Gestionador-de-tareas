9ac7fbf6484bb4e12dc4e9be802ea3b735066b2f
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-01 01:18:08 -0600

    feat: Creacion de la logica en LoginController.java

--- Gestionador_de_tareas/src/main/java/org/GUI/LoginController.java
+++ Gestionador_de_tareas/src/main/java/org/GUI/LoginController.java
@@ -1,1 +1,31 @@
 //aca haremos la logica de cada archivo para poder trabajar con javafx y que funcione
+
+package org.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML private TextField campoCorreo;
+    @FXML private PasswordField campoContrasena;
+
+    @FXML
+    private void handleLogin() {
+        String correo = campoCorreo.getText();
+        String contrasena = campoContrasena.getText();
+        System.out.println("Login con: " + correo);
+    }
+
+    @FXML
+    private void handleRecuperar(MouseEvent event) {
+        System.out.println("Recuperar contraseña");
+    }
+
+    @FXML
+    private void handleCrearCuenta(MouseEvent event) {
+        System.out.println("Crear cuenta");
+    }
+}
